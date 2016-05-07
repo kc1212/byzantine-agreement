d83d28505c89f5288842362f920bb4c352b4e2d5
kc1212 <kc1212@users.noreply.github.com>
2016-05-07 14:33:43 +0200

    Refactored main, support single node mode

--- src/Byzantine_Main.java
+++ src/Byzantine_Main.java
@@ -2,6 +2,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -18,48 +19,95 @@ public class Byzantine_Main {
 
     static void usage() {
         System.err.println("Invalid argument!");
-        System.err.println("usage: java Byzantine_Main <n> <f> [<omission|random|both>]");
+        System.err.println("usage: java Byzantine_Main \n\t<single|multi> <n> <f> <id - only for single mode> [<omission|random|both>]");
     }
 
-    public static void main(String args[]) {
-        if (args.length <= 3) {
+    static void startByzantine(int n, int f, List<String> addrs, int i, Byzantine.FailureType type) {
+        try {
+            int v = (new Random()).nextInt(2);
+            Byzantine byz = new Byzantine(i, n, f, v, addrs, type);
+            Naming.rebind(addrs.get(i), byz);
+            Thread.sleep(100); // wait for other nodes to come online
+            byz.run();
+        } catch (RemoteException | MalformedURLException | InterruptedException e) {
+            System.err.println("Failure!");
+            e.printStackTrace();
+        }
+    }
+
+    static Byzantine.FailureType parseFailureType(String str) {
+        Byzantine.FailureType type;
+        if (str.compareToIgnoreCase(Byzantine.FailureType.OMISSION.toString()) == 0) {
+            type = Byzantine.FailureType.OMISSION;
+        } else if (str.compareToIgnoreCase(Byzantine.FailureType.RANDOM.toString()) == 0) {
+            type = Byzantine.FailureType.RANDOM;
+        } else if (str.compareToIgnoreCase(Byzantine.FailureType.BOTH.toString()) == 0) {
+            type = Byzantine.FailureType.BOTH;
+        } else {
+            type = Byzantine.FailureType.NOFAILURE;
+            System.out.println("Invalid failure type, defaulting to NOFAILURE.");
+        }
+
+        return type;
+    }
+
+    static void handleSingle(String args[]) {
+        try {
+            int n = Integer.parseInt(args[0]);
+            int f = Integer.parseInt(args[1]);
+            List<String> addrs = makeAddrs(n);
+            int i = Integer.parseInt(args[2]);
+
+            Byzantine.FailureType type;
+            if (args.length >= 4)
+                type = parseFailureType(args[3]);
+            else
+                type = Byzantine.FailureType.NOFAILURE;
+
+            startByzantine(n, f, addrs, i, type);
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            usage();
+            return;
+        }
+    }
+
+    static void handleMultiple(String args[]) {
+        try {
             int n = Integer.parseInt(args[0]);
             int f = Integer.parseInt(args[1]);
             List<String> addrs = makeAddrs(n);
 
             Byzantine.FailureType type;
-            try {
-                if (args[2].compareToIgnoreCase(Byzantine.FailureType.OMISSION.toString()) == 0) {
-                    type = Byzantine.FailureType.OMISSION;
-                } else if (args[2].compareToIgnoreCase(Byzantine.FailureType.RANDOM.toString()) == 0) {
-                    type = Byzantine.FailureType.RANDOM;
-                } else if (args[2].compareToIgnoreCase(Byzantine.FailureType.BOTH.toString()) == 0) {
-                    type = Byzantine.FailureType.BOTH;
-                } else {
-                    usage();
-                    return;
-                }
-            } catch (IndexOutOfBoundsException e) {
+            if (args.length >= 3)
+                type = parseFailureType(args[2]);
+            else
                 type = Byzantine.FailureType.NOFAILURE;
-            }
 
             for (int i = 0; i < n; i++) {
                 final int I = i;
                 final Byzantine.FailureType Type = i < f ? type : Byzantine.FailureType.NOFAILURE;
-                Runnable task = () -> {
-                    try {
-                        int v = (new Random()).nextInt(2);
-                        Byzantine byz = new Byzantine(I, n, f, v, addrs, Type);
-                        Naming.rebind(addrs.get(I), byz);
-                        Thread.sleep(2); // wait for other nodes to come online
-                        byz.run();
-                    } catch (RemoteException | MalformedURLException | InterruptedException e) {
-                        System.err.println("Failure!");
-                        e.printStackTrace();
-                    }
-                };
+                Runnable task = () -> startByzantine(n, f, addrs, I, Type);
                 new Thread(task).start();
             }
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            usage();
+            return;
+        }
+    }
+
+    static String[] pop(String arr[]) {
+        return Arrays.copyOfRange(arr, 1, arr.length);
+    }
+
+    public static void main(String args[]) {
+        if (args[0].compareToIgnoreCase("multi") == 0) {
+            handleMultiple(pop(args));
+        } else if (args[0].compareToIgnoreCase("single") == 0) {
+            handleSingle(pop(args));
         } else {
             usage();
             return;
